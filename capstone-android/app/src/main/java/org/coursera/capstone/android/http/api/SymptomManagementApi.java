f104570bb6af4654127a254f1922dc59663b1c04
Mats Roshauw <karmats@gmail.com>
2015-02-24 23:13:26 +0100

    Uses google cloud computing as backend

--- capstone-android/app/src/main/java/org/coursera/capstone/android/http/api/SymptomManagementApi.java
+++ capstone-android/app/src/main/java/org/coursera/capstone/android/http/api/SymptomManagementApi.java
@@ -28,7 +28,7 @@ public interface SymptomManagementApi {
     public static final String USERNAME_PARAMETER = "username";
 
     // Service host
-    public static final String HOST = "https://192.168.1.76:8443";
+    public static final String HOST = "http://104.155.28.181:8080";
 
     // Login service oauth2
     public static final String TOKEN_PATH = "/oauth/token";
